c7d0136ec189c04da35a5d4d5644df1a9f1b849d
Iris Price <iris.orprecio.price@gmail.com>
2018-08-18 20:49:23 -0500

    I've put my own code for the functions in the assignment.
    The file has been overwritten with my code for the assigned functions. I have also added brief descriptions for both.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below when used in conjunction
+## with each other, uses lexical scoping and the 
+## assignment symbol <<- to cache inverses of 
+## matrices and retrieve them when needed, or 
+## compute inverses of matrices that aren't cached.
 
-## Write a short comment describing this function
+## This first function takes a matrix and returns 
+## something like a new "data type" where the 
+## matrix fed into it is now attached with a list of
+## four things, the setter and getter of the said 
+## function, and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  setmat <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  getmat <- function() x
+  setinv <- function(matinv) inv <<- matinv
+  getinv <- function() inv
+  list(setmat = setmat, getmat = getmat,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This second function takes as argument an object
+## that has essentially "data type" set by the above
+## function. It returns either a cached inverse 
+## (if it has been previously computed) or computes
+## the inverse and returns that.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  newmat <- x$getmat()
+  inv <- solve(newmat, ...)
+  x$setinv(inv)
+  inv
 }
